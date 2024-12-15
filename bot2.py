084c6ed4ed20a2054fe53274900a504d1ec75d67
bienvenu-cyber <80950358+bienvenu-cyber@users.noreply.github.com>
2024-12-15 09:37:33 +0100

    Update bot2.py
    3?

--- bot2.py
+++ bot2.py
@@ -73,9 +73,9 @@ def fetch_historical_data(crypto_symbol, currency="USD", interval="hour", limit=
     # Déterminer le bon endpoint en fonction de l'intervalle
        if interval == "hour":
         endpoint = "histohour"
-    elif interval == "day":
+       elif interval == "day":
         endpoint = "histoday"
-    else:
+       else:
         raise ValueError("Intervalle non supporté. Utilisez 'hour' ou 'day'.")
     
     url = f"{base_url}{endpoint}"
